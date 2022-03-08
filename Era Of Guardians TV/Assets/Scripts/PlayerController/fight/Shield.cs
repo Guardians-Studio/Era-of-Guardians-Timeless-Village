553cdd984e7aa26e8b2d866daca116bc4ae29689
Guimauve <guillaume.jolivalt@epita.fr>
2022-03-08 15:52:45 +0100

    Trying to merge

--- Era Of Guardians TV/Assets/Scripts/PlayerController/fight/Shield.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Shield : MonoBehaviour
-{
-    public AudioClip swordAttack;
-    public bool isDefending;
-}
