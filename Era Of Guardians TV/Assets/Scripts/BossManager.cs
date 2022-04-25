65e7fb7e8550a15056602206b86b5a2823e74eb0
Raphaël Heng <raphael.heng@epita.fr>
2022-04-25 19:33:06 +0200

    boss<Unity>: endscreen + return menu complete

--- Era Of Guardians TV/Assets/Scripts/BossManager.cs
+++ Era Of Guardians TV/Assets/Scripts/BossManager.cs
@@ -1,17 +1,25 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class BossManager : MonoBehaviour
 {
     [SerializeField] Transform statue;
     [SerializeField] GameObject bossPrefab;
+    [SerializeField] Player player;
 
     private bool spawned = false;
 
     public GameObject endscreenPanel;
     public Text endscreenText;
+
+    private void Start()
+    {
+        endscreenPanel.SetActive(false);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -29,10 +37,22 @@ public class BossManager : MonoBehaviour
 
     private void Update()
     {
-        if (GameObject.Find("FinalBoss") == null && spawned)
+        if (GameObject.Find("FinalBoss") == null)
         {
             endscreenPanel.SetActive(true);
             endscreenText.text = "Tidalar est vaincu, bien joué !";
+            player.GetComponent<PlayerLook>().enabled = false;
+            player.GetComponent<WeaponController>().enabled = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
+    }
+
+    public void QuitGame()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene("MainMenu");
+        PhotonNetwork.Disconnect();
     }
 }
