13ac3465e7e6393c51f4d16cc759fcf31625392d
Raphaël Heng <raphael.heng@epita.fr>
2022-04-25 19:06:25 +0200

    build<Unity>: saving

--- Era Of Guardians TV/Assets/Scripts/BossManager.cs
+++ Era Of Guardians TV/Assets/Scripts/BossManager.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossManager : MonoBehaviour
 {
     [SerializeField] Transform statue;
     [SerializeField] GameObject bossPrefab;
+
+    private bool spawned = false;
+
+    public GameObject endscreenPanel;
+    public Text endscreenText;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,7 +22,17 @@ public class BossManager : MonoBehaviour
             {
                 Instantiate(bossPrefab, statue);
                 Destroy(statue.gameObject);
+                spawned = true;
             }
         }  
+    }
+
+    private void Update()
+    {
+        if (GameObject.Find("FinalBoss") == null && spawned)
+        {
+            endscreenPanel.SetActive(true);
+            endscreenText.text = "Tidalar est vaincu, bien joué !";
+        }
     }
 }
