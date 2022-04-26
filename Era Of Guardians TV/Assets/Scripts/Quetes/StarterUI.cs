30ba6868dc8cf727fa609fa9ba00253a4ad73a10
R-Onne <lesech.erwann@gmail.com>
2022-04-26 12:31:30 +0200

    build<Unity>: playerPrefab - add starter menu

---
+++ Era Of Guardians TV/Assets/Scripts/Quetes/StarterUI.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarterUI : MonoBehaviour
+{
+    [SerializeField] GameObject firstPanel;
+    [SerializeField] GameObject secondPanel;
+
+    private bool transition = false;
+
+    private void Start()
+    {
+        firstPanel.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.F) && firstPanel.activeSelf)
+        {
+            firstPanel.SetActive(false);
+            secondPanel.SetActive(true);
+            StartCoroutine(Transition());
+        }
+        else if (transition && Input.GetKey(KeyCode.F) && secondPanel.activeSelf)
+        {
+            secondPanel.SetActive(false);
+            this.enabled = false;
+            print("second");
+        }
+    }
+
+    IEnumerator Transition()
+    {
+        yield return new WaitForSeconds(1f);
+        transition = true;
+    }
+}
