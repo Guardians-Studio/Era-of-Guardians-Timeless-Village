c05b7dc870d208f846b2a6eedac762c20fba8e18
Guimauve <guillaume.jolivalt@epita.fr>
2022-05-31 18:18:16 +0200

    okeanos finished

---
+++ Era Of Guardians TV/Assets/Scripts/Water/Water.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    /*private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponentInParent<Player>
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }*/
+}
